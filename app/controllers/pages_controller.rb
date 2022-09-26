a5633b57239a775377c5b456a60b59aa7158d2ec
Leith Atia <leith.atia@gmail.com>
2022-09-26 17:53:35 +0100

    Refactor PagesController code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,35 +1,51 @@
 # Home controller
 class PagesController < ApplicationController
   def index
-    require "nokogiri"
-    require "open-uri"
+    read_stocks
+  end
 
-    url = "https://www.tradingview.com/markets/stocks-usa/market-movers-large-cap/"
+  def about
+  end
 
-    html_file = URI.open(url).read
-    html_doc = Nokogiri::HTML(html_file)
-    table = html_doc.at(".table-DR3mi0GH")
-    rows = table.search("tr")
+  private
 
+  def read_stocks
+    # Get all rows from html table containing stock info
     @stocks = []
+    rows = parse_html
 
+    # Grab stock data from each row leaving out table header
     rows.drop(1).each do |row|
       company_name = row.search(".tickerDescription-hMpTPJiS")
       company_logo = row.search(".tv-circle-logo").attr('src')
       stock_price = row.search(".right-TKkxf89L")
       change_1d_percentage = row.search(".positive-C2C2Vilj")
       change_1d_currency = row.search(".positive-avn2kVRm")
-
-      @stocks.push({
-                     name: company_name.text.strip,
-                     logo_URL: company_logo,
-                     price: stock_price.text.strip.match(/\d*.\d*(?=USD)/),
-                     change_percent: change_1d_percentage.text.strip,
-                     change_currency: change_1d_currency.text.strip
-                   })
+      write_stocks(company_name, company_logo, stock_price, change_1d_percentage, change_1d_currency)
     end
   end
 
-  def about
+  def parse_html
+    require "nokogiri"
+    require "open-uri"
+
+    url = "https://www.tradingview.com/markets/stocks-usa/market-movers-large-cap/"
+
+    html_file = URI.parse(url).open.read
+    html_doc = Nokogiri::HTML(html_file)
+
+    # Find table containing stock data through CSS class and return rows
+    table = html_doc.at(".table-DR3mi0GH")
+    table.search("tr")
+  end
+
+  def write_stocks(company_name, company_logo, stock_price, change_1d_percentage, change_1d_currency)
+    @stocks.push({
+                   name: company_name.text.strip,
+                   logo_URL: company_logo,
+                   price: stock_price.text.strip.match(/\d*.\d*(?=USD)/),
+                   change_percent: change_1d_percentage.text.strip,
+                   change_currency: change_1d_currency.text.strip
+                 })
   end
 end
