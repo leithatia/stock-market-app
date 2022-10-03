df12da84cb2c778761391cd4f3e47ed95e6661c9
Leith Atia <leith.atia@gmail.com>
2022-10-03 15:42:25 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-puts "Destroying DB records..."
-User.detroy_all
-Stock.destroy_all
+puts 'Destroying DB records...'
 Portfolio.destroy_all
+User.destroy_all
+Stock.destroy_all
 
-puts "Creating users..."
-leith = User.create!(email)
+puts 'Creating users...'
+leith = User.create!(username: 'Leith', email: 'leith.atia@gmail.com', password: 'password')
+jim = User.create!(username: 'Jim', email: 'jim@gmail.com', password: 'password')
+
+puts 'Creating stocks...'
+leith.stocks.create!(ticker: 'AAPL')
+leith.stocks.create!(ticker: 'MSFT')
+leith.stocks.create!(ticker: 'TSLA')
+leith.stocks.create!(ticker: 'HPE')
+leith.stocks.create!(ticker: 'SONY')
+
+jim.stocks.create!(ticker: 'HYMFT')
+jim.stocks.create!(ticker: 'MSFT')
+jim.stocks.create!(ticker: 'RACE')
+jim.stocks.create!(ticker: 'HPE')
+
+puts 'Done!'
