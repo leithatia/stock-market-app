166b4da522d7b79f4d53ee518c9869c035bf71fd
Leith Atia <leith.atia@gmail.com>
2022-10-06 13:01:39 +0100

    Edit seed file to remove Stock and Portfolio refs

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,6 @@ require 'csv'
 # puts csv_text
 
 puts 'Destroying DB records...'
-Portfolio.destroy_all
 User.destroy_all
 Stock.destroy_all
 
@@ -12,23 +11,23 @@ puts 'Creating users...'
 leith = User.create!(username: 'Leith', email: 'leith.atia@gmail.com', password: 'password')
 jim = User.create!(username: 'Jim', email: 'jim@gmail.com', password: 'password')
 
-puts 'Creating stocks...'
-CSV.foreach('lib/seeds/nasdaq_screener_1664808689118.csv', headers: true) do |row|
-  stock_symbol = row[0].to_s.gsub(/[\/\^]/, '.')
-  stock_name = row[1].to_s
-  Stock.create!(symbol: stock_symbol, name: stock_name)
-end
+# puts 'Creating stocks...'
+# CSV.foreach('lib/seeds/nasdaq_screener_1664808689118.csv', headers: true) do |row|
+#   stock_symbol = row[0].to_s.gsub(/[\/\^]/, '.')
+#   stock_name = row[1].to_s
+#   Stock.create!(symbol: stock_symbol, name: stock_name)
+# end
 
 puts 'Building portfolios...'
-leith.stocks << Stock.find_by(symbol: 'AAPL')
-leith.stocks << Stock.find_by(symbol: 'MSFT')
-leith.stocks << Stock.find_by(symbol: 'HPE')
-leith.stocks << Stock.find_by(symbol: 'SONY')
-leith.stocks << Stock.find_by(symbol: 'RACE')
+# leith.stocks << Stock.find_by(symbol: 'AAPL')
+# leith.stocks << Stock.find_by(symbol: 'MSFT')
+# leith.stocks << Stock.find_by(symbol: 'HPE')
+# leith.stocks << Stock.find_by(symbol: 'SONY')
+# leith.stocks << Stock.find_by(symbol: 'RACE')
 
-jim.stocks << Stock.find_by(symbol: 'TSLA')
-jim.stocks << Stock.find_by(symbol: 'MSFT')
-jim.stocks << Stock.find_by(symbol: 'AAPL')
-jim.stocks << Stock.find_by(symbol: 'HPE')
+# jim.stocks << Stock.find_by(symbol: 'TSLA')
+# jim.stocks << Stock.find_by(symbol: 'MSFT')
+# jim.stocks << Stock.find_by(symbol: 'AAPL')
+# jim.stocks << Stock.find_by(symbol: 'HPE')
 
 puts 'Done!'
